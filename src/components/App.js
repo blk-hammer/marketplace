44d6d36e1221fabe588195a0d583ff90f1a606a3
Rishabh Agnihotri <rishabhagnihotri1999@gmail.com>
2020-07-31 12:10:36 +0530

    minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Web3 from "web3";
-import "./App.css";
+// import "./App.css";
 import Marketplace from "../abis/Marketplace.json";
 import Navbar from "./NavBar";
 import Main from "./Main";
